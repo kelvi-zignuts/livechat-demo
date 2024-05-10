d76145188154898cee3b8d134942c369c4cf2cb6
kelvi-zignuys <kelvib@zignuts.com>
2024-05-10 19:18:14 +0530

    create live chat application in livewire

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Livewire\Chat\Chat;
+use App\Livewire\Chat\Index;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+require __DIR__.'/auth.php';
+
+Route::get('/chat',[Index::class,'render'])->name('chat.index');
+Route::get('/chat/{query}',[Chat::class,'render'])->name('chat');
+
